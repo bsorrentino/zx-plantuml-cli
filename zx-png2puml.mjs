e7a11965f1d592b1c1bd292be439911ab57c7d18
bsorrentino <bartolomeo.sorrentino@gmail.com>
2022-03-03 13:04:17 +0100

    feat(png2puml): initial version

---
+++ zx-png2puml.mjs
@@ -0,0 +1,50 @@
+#!/usr/bin/env zx
+
+import 'zx/globals'
+
+
+const PLANTUML_RUNTIME = 'plantuml'
+
+
+
+const getVersion = async () => 
+    await $`${PLANTUML_RUNTIME} -version`
+
+
+export const askForImageFileName = async () => {
+    if( argv.image ) {
+        return argv.image
+    }
+    
+    return await question('image path: ')
+
+}
+
+/**
+ * cli for raw command: 
+ * java -jar plantuml.jar -encodesprite 16 $1.png > $1.puml
+ */
+async function main() {
+
+    await getVersion()
+    
+    $.verbose = false
+
+    const image = await askForImageFileName()
+
+    const basename = `${path.basename(image)}.puml`
+
+    let outfile = basename
+    if (argv.out) {
+        await fs.ensureDir(argv.out)
+        outfile = path.join(argv.out, basename)
+    }
+
+    const outstream = fs.createWriteStream(outfile)
+
+    await $`${PLANTUML_RUNTIME} -encodesprite 16 ${image}`.pipe(outstream)
+
+
+}
+
+main()
